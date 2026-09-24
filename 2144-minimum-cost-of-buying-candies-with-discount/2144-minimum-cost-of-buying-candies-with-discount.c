4bdd7c77ddbef0e9c7911eadc57002306c06506b
Akshit Sharma <akshubhardwaj231@gmail.com>
2026-09-24 14:15:10 +0530

    Time: 3 ms (12.68%), Space: 8.9 MB (97.18%) - LeetHub-Neo

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.c
@@ -0,0 +1,17 @@
+int cmp(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+int minimumCost(int* cost, int costSize) {
+    qsort(cost, costSize, sizeof(int), cmp);
+    int total = 0;
+    int take = 0;
+    for (int i = costSize - 1; i >= 0; i--) {
+        if (take == 2) {
+            take = 0;
+        } else {
+            total += cost[i];
+            take++;
+        }
+    }
+    return total;
+}
