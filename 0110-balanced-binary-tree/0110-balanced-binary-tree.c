01419f5828a931e52c9317e42036fecdeaa51121
Akshit Sharma <akshubhardwaj231@gmail.com>
2026-09-24 17:51:39 +0530

    Time: 0 ms (100%), Space: 12.5 MB (100%) - LeetHub-Neo

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int checkHeight(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftH = checkHeight(root->left);
+    if (leftH == -1) return -1; // left subtree unbalanced
+
+    int rightH = checkHeight(root->right);
+    if (rightH == -1) return -1; // right subtree unbalanced
+
+    if (abs(leftH - rightH) > 1) 
+        return -1; // this node is unbalanced
+
+    return (leftH > rightH ? leftH : rightH) + 1;
+}
+
+bool isBalanced(struct TreeNode* root){
+    return checkHeight(root) != -1;
+}
