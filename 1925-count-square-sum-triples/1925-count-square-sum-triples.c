5973093736d7787cb0dd8513b147bd17485df1e1
Akshit Sharma <akshubhardwaj231@gmail.com>
2026-09-24 18:06:49 +0530

    Time: 3 ms (87.76%), Space: 7.8 MB (100%) - LeetHub-Neo

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.c
@@ -0,0 +1,17 @@
+#include <math.h>
+
+int countTriples(int n) {
+    int ans = 0;
+
+    for (int a = 1; a <= n; a++) {
+        for (int b = 1; b <= n; b++) {
+            int c2 = a*a + b*b;
+            int c = (int)sqrt(c2);
+            if (c*c == c2 && c <= n) {
+                ans++;
+            }
+        }
+    }
+
+    return ans;
+}
