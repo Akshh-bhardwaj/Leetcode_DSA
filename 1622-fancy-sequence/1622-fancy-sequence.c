f4e1c1cc6f18d6f74d240fc0eaf17073b2d56595
Akshit Sharma <akshubhardwaj231@gmail.com>
2026-09-24 17:45:25 +0530

    Time: 58 ms (90.91%), Space: 141.4 MB (9.09%) - LeetHub-Neo

---
+++ 1622-fancy-sequence/1622-fancy-sequence.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+#define MOD 1000000007
+#define MAXN 100005
+
+long long modpow(long long a,long long b){
+    long long r=1;
+    while(b){
+        if(b&1) r=r*a%MOD;
+        a=a*a%MOD;
+        b>>=1;
+    }
+    return r;
+}
+
+typedef struct{
+    long long arr[MAXN];
+    int sz;
+    long long mul,add;
+} Fancy;
+
+Fancy* fancyCreate(){
+    Fancy* f=malloc(sizeof(Fancy));
+    f->sz=0;
+    f->mul=1;
+    f->add=0;
+    return f;
+}
+
+void fancyAppend(Fancy* f,int val){
+    long long inv=modpow(f->mul,MOD-2);
+    long long v=(val-f->add)%MOD;
+    if(v<0) v+=MOD;
+    f->arr[f->sz++]=v*inv%MOD;
+}
+
+void fancyAddAll(Fancy* f,int inc){
+    f->add=(f->add+inc)%MOD;
+}
+
+void fancyMultAll(Fancy* f,int m){
+    f->mul=f->mul*m%MOD;
+    f->add=f->add*m%MOD;
+}
+
+int fancyGetIndex(Fancy* f,int idx){
+    if(idx>=f->sz) return -1;
+    return (f->arr[idx]*f->mul+f->add)%MOD;
+}
+
+void fancyFree(Fancy* f){
+    free(f);
+}
