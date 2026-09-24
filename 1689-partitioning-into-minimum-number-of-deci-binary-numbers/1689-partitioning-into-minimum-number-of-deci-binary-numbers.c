19c70e9313a81c9f162fb6938e35e2406071284e
Akshit Sharma <akshubhardwaj231@gmail.com>
2026-09-24 17:47:49 +0530

    Time: 4 ms (28.21%), Space: 11.2 MB (7.69%) - LeetHub-Neo

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.c
@@ -0,0 +1,8 @@
+int minPartitions(char * n) {
+    int max = 0;
+    for (int i = 0; n[i] != '\0'; i++) {
+        int digit = n[i] - '0';
+        if (digit > max) max = digit;
+    }
+    return max;
+}
